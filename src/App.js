8740e0d0054c5239d530b4edac9662ee37075e67
SiEr0212 <simonertel@gmx.de>
2021-05-07 20:42:10 +0200

    added second route

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ function App() {
               return <h1>Welcome Home</h1>;
             }}
           ></Route>
+          <Route
+            path="/about"
+            render={() => {
+              return <h1>Welcome About Page</h1>;
+            }}
+          ></Route>
         </header>
       </div>
     </BrowserRouter>
