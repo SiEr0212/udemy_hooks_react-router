a01cce39ba651604fc716fb8499f77aede5d79ad
SiEr0212 <simonertel@gmx.de>
2021-05-07 20:37:42 +0200

    added first route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, {  } from "react";
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <Route
+            path="/"
+            render={() => {
+              return <h1>Welcome Home</h1>;
+            }}
+          ></Route>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
