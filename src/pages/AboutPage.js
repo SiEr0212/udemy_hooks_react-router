af336b82ad7a16ca1a1715d0026303422ebc769f
SiEr0212 <simonertel@gmx.de>
2021-05-09 19:20:56 +0200

    adding useContext

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import messageContext from "../contexts/messageContext";
 
 export default function AboutPage() {
-  return <h1>Welcome About Page</h1>;
+  return (
+    <>
+      <h1>Welcome About Page</h1>
+      <h2>Message:{useContext(messageContext)}</h2>
+    </>
+  );
 }
